e9c7d555fc384900c27d02062b12978d73baec98
Dmitrii Kilishek <mentalclear@gmail.com>
2022-03-07 22:00:05 -0500

    First commit here, but all done

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1646596549247-e3972347715a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646640727785-1b3350f70b39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646618178545-0cbba20f124c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646593449705-f6d7197ce076?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1033&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646624679259-4f6a41d01568?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+]
